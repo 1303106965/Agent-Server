a49185412c99cc25f1e7a191b85e9b94df30d306
Violet <violetEkko@proton.me>
2026-04-27 11:43:59 +0800

    remove mistakenly ai

--- server/ai/llm.ts
+++
@@ -1,6 +0,0 @@
-import OpenAI from 'openai'
-
-export const client = new OpenAI({
-  apiKey: 'sk-3bfffbd0b70b4eaea5ce4e3e26cbc2b3',
-  baseURL: 'https://dashscope.aliyuncs.com/compatible-mode/v1'
-})
